790efd5dd050a299c556f895aac5b8708efc106b
Gabriel Yap <glyap@ucdavis.edu>
2023-12-06 20:15:19 -0800

    SQL on pricing
    Using phpymyadmin, I can make an sql database and connect to it in the app. The api/mysql/products/route makes the connection, and it's called in pricing/page.jsx. Nextconfig irrelevant

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+        host: 'localhost',
+        port: 3306,
+        user: 'root', // Password comes after this but i just like blank ''
+        database: 'nikeshoes',
+    }
+
+}
 
 module.exports = nextConfig
